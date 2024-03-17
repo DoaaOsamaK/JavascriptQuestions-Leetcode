87fa0d3509bc3427bfcf17f8b8bac1e4f1d282cc
Doa'a OsamaK <147305995+DoaaOsamaK@users.noreply.github.com>
2024-03-17 10:58:17 +0200

    Time: 132 ms (30.42%), Space: 73.2 MB (32.26%) - LeetHub

---
+++ 2631-group-by/2631-group-by.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @return {Array}
+ */
+Array.prototype.groupBy = function(fn) {
+  // Reduce the array into a single object
+  return this.reduce((grouped, item) => {
+    // Apply the provided callback function to get the key
+    const key = fn(item);
+    
+    // If the key doesn't exist in the grouped object, create a new array for it
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+    
+    // Push the current item to the array associated with the key
+    grouped[key].push(item);
+    
+    // Return the updated grouped object for the next iteration
+    return grouped;
+  }, {});
+};
+
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
